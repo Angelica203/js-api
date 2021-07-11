9a6d535a1fecf482287351a25a30914d3321183c
github username <github email address>
2021-07-11 10:26:32 -0400

    adding api to repo

---
+++ smoothies_api/app/models/review.rb
@@ -0,0 +1,3 @@
+class Review < ApplicationRecord
+    belongs_to :smoothie
+end
---
+++ smoothies_api/db/seeds.rb
@@ -0,0 +1,8 @@
+smoothies = Smoothie.create(flavor: "Green Hulk", image: "https://images.pexels.com/photos/1346347/pexels-photo-1346347.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260")
+smoothies = Smoothie.create(flavor: "Captain Berry", image: "https://cdn.pixabay.com/photo/2020/01/09/04/21/smoothie-4751820_1280.jpg")
+smoothies = Smoothie.create(flavor: "Mango King", image: "https://cdn.pixabay.com/photo/2017/05/05/01/50/smoothie-2285722_1280.jpg")
+smoothies = Smoothie.create(flavor: "Coco Loco", image:)
+
+reviews = Review.create(comment: "this smoothie is very Good")
+reviews = Review.create(comment: "The captain berry is my favorite.")
+reviews = Review.create(comment: "Mango king is my first favorite and Green hulk is my second.")
